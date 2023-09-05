1a1f4059180a8b6544e4d925889c37186ab95052
Student <Student@192.168.3.103>
2023-09-05 17:29:18 +0800

    Quiz 1

--- src/jalique_quiz1/JALIQUE_QUIZ1.java
+++ src/jalique_quiz1/JALIQUE_QUIZ1.java
@@ -5,6 +5,9 @@
  */
 package jalique_quiz1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author Student
@@ -15,7 +18,92 @@ public class JALIQUE_QUIZ1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Nigger");
+        System.out.println("Choose quiz number");
+       Scanner scn = new Scanner(System.in);
+       int choice = scn.nextInt();
+       switch (choice){
+           case 1:
+               printArray();
+               break;
+           case 2:
+               System.out.println("Wala koy 2 :( ");
+               break;
+           case 3:
+               Num3();
+               break;
+       }
+        
+        
+       
+        }
+        
+    
+    
+    
+    public static void printArray(){
+
+       int arr[] = {82,44,96,7,13,56,74,14,98,36};
+        System.out.println(" #1 Default Array: " + Arrays.toString(arr));
+        
+}
+    public static void Num2() {
+        System.out.println("Enter number of array elements:");
+         Scanner scanner = new Scanner(System.in);
+        int elemArr = scanner.nextInt();
+        int[] newArr = new int[elemArr];
+            
+        System.out.println("Enter Array Elements");
+        for (int i = 0; i < elemArr; i++) {
+            newArr[i]=scanner.nextInt();
+        }
+        
+        System.out.println("Array created: " + Arrays.toString(newArr));
+        System.out.println("Delete an element: ");
+        int deleteElem = scanner.nextInt();
+        
+        for (int i = 0; i <= deleteElem; i++) {
+            newArr[i - 1]= 
+        }
     }
     
-}
+    public static void Num3() {
+        System.out.println("Enter number of array elements:");
+         Scanner scanner = new Scanner(System.in);
+        int elemArr = scanner.nextInt();
+        int[] newArr = new int[elemArr];
+            
+        System.out.println("Enter Array Elements");
+        for (int i = 0; i < elemArr; i++) {
+            newArr[i]=scanner.nextInt();
+             }
+        System.out.println("Array" + Arrays.toString(newArr));
+        
+        
+        insertionSort(newArr);
+        System.out.println("Sorted Array" + Arrays.toString(newArr));
+       
+            
+        }
+     public static void insertionSort(int arr[]){
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int insert = arr[i];
+            int j = i -1;
+            
+            while (j >= 0 && arr[j] > insert) {                
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            
+            arr[j + 1] = insert;
+            
+        }
+    }
+   
+    
+    
+   
+    }
+
+    
+
